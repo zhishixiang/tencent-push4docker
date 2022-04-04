e610ae5c03efa1109c47d5e4b4b89e8887ef5cc3
zhishixiang <1015256551@qq.com>
2022-04-04 19:28:17 +0800

    处理心跳信号

--- QQ.py
+++ QQ.py
@@ -1,10 +1,15 @@
-from flask import Flask
-
+import json
+from tokenize import group
+from flask import Flask,request,jsonify
+import json
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
+@app.route("/",methods=['POST'])
+def recvMsg():
+    data = request.get_data()
+    json_data = json.loads(data.decode("utf-8"))
+    if json_data["post_type"] == "meta_event":
+        if json_data["meta_event_type"] == "heartbeat":
+            print("接收心跳信号成功")
     return "<p>dd</p>"
-@app.route("/asd")
-def hello_world1():
-    return "666"
+    
